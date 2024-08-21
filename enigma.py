2857d80ca1595b761f565fcf7e541d8e04d59fd6
eden-avidan <eden.avidan2000@gmail.com>
2024-08-21 13:34:14 +0300

    Implementing encrypt_char

--- enigma.py
+++ enigma.py
@@ -1,15 +1,59 @@
 import copy
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
-        self. hash_map = hash_map
+        self.hash_map = hash_map
         self.wheels = wheels
         self.reflector_map = reflector_map
 
     def encrypt(self, message):
         pass
 
-    def encrypt_char(self, char):
-        tmp_wheels = self.wheels
+
+    def encrypt_char(self, char, tmp_wheels):
+        # Local variables
+        W1_MULT = 2
+        W1_IDX = 0
+        W2_IDX = 1
+        W3_IDX = 2
+        DEFINED_MOD = 26
+
+        # Check if encryption is not needed
+        if char.islower() is False:
+            result = (char, 0)
+            return result
+
+        # Encryption is needed. start the process
+        i = self.hash_map[char]
+        mod_result = (((tmp_wheels[W1_IDX] * W1_MULT) - tmp_wheels[W2_IDX] +
+                       tmp_wheels[W3_IDX]) % DEFINED_MOD)
+        if mod_result == 0:
+            i += 1
+        else:
+            i += mod_result
+        i = i % DEFINED_MOD
+
+        c1 = i
+        c2 = ''
+        for key in self.reflector_map.keys():
+            if self.reflector_map[key] == c1:
+                c2 = key
+                break
+
+        i = self.hash_map[c2]
+        if mod_result == 0:
+            i -= 1
+        else:
+            i -= mod_result
+        i = i % DEFINED_MOD
+
+        c3 = ''
+        for key in self.hash_map.keys():
+            if self.hash_map[key] == i:
+                c3 = key
+                break
+
+        result = (c3, 1)
+        return result
 
 
 def load_enigma_from_path(path):

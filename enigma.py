42dc860ef0618e74ec735ec3fdc388b5eaa8b228
eden-avidan <eden.avidan2000@gmail.com>
2024-08-21 14:30:29 +0300

    Implementing load_enigma_from_path, fixint errors in wheels func

--- enigma.py
+++ enigma.py
@@ -1,4 +1,10 @@
 import copy
+import json
+
+W1_IDX = 0
+W2_IDX = 1
+W3_IDX = 2
+
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
         self.hash_map = hash_map
@@ -13,16 +19,13 @@ class Enigma:
             encrypted_char, add_to_count = self.encrypt_char(char, tmp_wheels)
             encrypted_list.append(encrypted_char)
             count += add_to_count
-            self.rotate_wheels(count, tmp_wheels)
+            rotate_wheels(count, tmp_wheels)
         return ''.join(encrypted_list)
 
 
     def encrypt_char(self, char, tmp_wheels):
         # Local variables
         W1_MULT = 2
-        W1_IDX = 0
-        W2_IDX = 1
-        W3_IDX = 2
         DEFINED_MOD = 26
 
         # Check if encryption is not needed
@@ -40,12 +43,15 @@ class Enigma:
             i += mod_result
         i = i % DEFINED_MOD
 
-        c1 = i
-        c2 = ''
-        for key in self.reflector_map.keys():
-            if self.reflector_map[key] == c1:
-                c2 = key
+        # match char to c1 from hash that matches i
+        c1 = ''
+        for key in self.hash_map.keys():
+            if self.hash_map[key] == i:
+                c1 = key
                 break
+
+        # match char to c2 from refl that matches c1
+        c2 = self.reflector_map[c1]
 
         i = self.hash_map[c2]
         if mod_result == 0:
@@ -65,13 +71,31 @@ class Enigma:
         return result
 
 def rotate_wheels(count, tmp_wheels):
-    tmp_wheels[0] = 1 if tmp_wheels[0] > 8 else tmp_wheels[0] + 1
-    tmp_wheels[1] *= 2 if tmp_wheels[1] % 2 == 1 else tmp_wheels[1] - 1
-    if tmp_wheels[2] % 10 == 0:
-        tmp_wheels[2] = 10
-    elif tmp_wheels[2] % 3 == 0:
-        tmp_wheels[2] = 5
-    else: tmp_wheels[2] = 0
+    tmp_wheels[W1_IDX] = 1 if tmp_wheels[W1_IDX] == 8\
+        else tmp_wheels[W1_IDX] + 1
+    tmp_wheels[W2_IDX] = tmp_wheels[W2_IDX] * 2 if count % 2 == 0\
+        else tmp_wheels[W2_IDX] - 1
+    if count % 10 == 0:
+        tmp_wheels[W3_IDX] = 10
+    elif count % 3 == 0:
+        tmp_wheels[W3_IDX] = 5
+    else: tmp_wheels[W3_IDX] = 0
+
+
+class JSONFileError(Exception):
+    def __init__(self, message="JSONFileError occurred"):
+        self.message = message
+        super().__init__(self.message)
+
 
 def load_enigma_from_path(path):
-    pass
+    try:
+        with open(path, 'r') as file:
+            maps = json.load(file)
+    except Exception:
+        raise JSONFileError(path)
+
+    hash_map = maps['hash_map']
+    wheels = maps['wheels']
+    reflector_map = maps['reflector_map']
+    return Enigma(hash_map, wheels, reflector_map)

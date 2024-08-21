c1c09521e55b7d37709e4470d9ca4552d2f07437
Guy Harel <guy.harel24@gmail.com>
2024-08-21 12:40:58 +0300

    pushing files

---
+++ enigma.py
@@ -0,0 +1,16 @@
+import copy
+class Enigma:
+    def __init__(self, hash_map, wheels, reflector_map):
+        self. hash_map = hash_map
+        self.wheels = wheels
+        self.reflector_map = reflector_map
+
+    def encrypt(self, message):
+        pass
+
+    def encrypt_char(self, char):
+        tmp_wheels = self.wheels
+
+
+def load_enigma_from_path(path):
+    pass
